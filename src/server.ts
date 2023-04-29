bba995eca746aa701c52cd0604087b7267d84e62
Vinícius Miranda <viniciusc.miranda@oulook.com>
2023-04-29 04:46:56 -0300

    adding fastify cors

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,57 @@
 import fastify from "fastify";
+import cors from "@fastify/cors";
 import z from "zod";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Prisma } from "@prisma/client";
 
 const app = fastify();
 
-const prisma = new PrismaClient();
+app.register(cors, { origin: true }).then(() => {
+  const prisma = new PrismaClient();
 
-app.get("/users", async () => {
-  const users = await prisma.user.findMany();
-  return { users };
-});
-
-app.post("/users", async (request, reply) => {
-  const createUserSchema = z.object({
-    name: z.string(),
-    email: z.string().email(),
+  app.get("/users", async () => {
+    const users = await prisma.user.findMany();
+    return { users };
   });
 
-  const { name, email } = createUserSchema.parse(request.body);
+  app.post("/users", async (request, reply) => {
+    const createUserSchema = z.object({
+      name: z.string(),
+      email: z.string().email(),
+    });
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-    },
+    const { name, email } = createUserSchema.parse(request.body);
+
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+      },
+    });
+
+    return reply.status(201).send();
   });
 
-  return reply.status(201).send();
+  app.delete("/users/:userId", async (request, reply) => {
+    const deleteUserSchema = z.object({
+      userId: z.string(),
+    });
+
+    const { userId } = deleteUserSchema.parse(request.params);
+
+    try {
+      await prisma.user.delete({ where: { id: userId } });
+    } catch (err) {
+      if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        if (err.code === "P2025") {
+          return reply.status(404).send("User not found.");
+        }
+      }
+
+      throw err;
+    }
+
+    return reply.status(204).send();
+  });
 });
 
 app
